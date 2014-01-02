942812cf34ebb5f93f2a02f9a4fdc5438d1e57d9
Steve Lacy <me@slacy.me>
2014-01-02 10:32:07 -0700

    Add error check if file is null or is a stream

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ module.exports = function(options) {
   // Map each file to this function
   function hamlStream(file, cb) {
     // Remember that contents is ALWAYS a buffer
+    if (file.isNull()) return cb(null, file); // pass along
+    if (file.isStream()) return cb(new Error("gulp-haml: Streaming not supported"));
+
     var html = haml.render(file.contents.toString("utf8"));
     file.path = gutil.replaceExtension(file.path, '.html');
     file.contents = new Buffer(html);
