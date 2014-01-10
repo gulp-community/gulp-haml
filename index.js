0e33377cb8dadfbaf776b2f92a57526265f80e51
Steve Lacy <me@slacy.me>
2014-01-10 09:09:18 -0700

    Add changes and update dependencies

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var map = require('map-stream');
 var gutil = require('gulp-util');
-var path = require('path');
 var haml = require('haml');
 
 module.exports = function(options) {
-
+  if(!options) options = {};
 
   // Map each file to this function
   function hamlStream(file, cb) {
@@ -12,7 +11,7 @@ module.exports = function(options) {
     if (file.isNull()) return cb(null, file); // pass along
     if (file.isStream()) return cb(new Error("gulp-haml: Streaming not supported"));
 
-    var html = haml.render(file.contents.toString("utf8"));
+    var html = haml.render(file.contents.toString("utf8"), options);
     file.path = gutil.replaceExtension(file.path, '.html');
     file.contents = new Buffer(html);
 
