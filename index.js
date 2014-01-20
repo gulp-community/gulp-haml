950cfa2a641bd9e3e951920c8fab5c2c4544feb2
Corbacho <davidcorbacho@gmail.com>
2014-01-20 15:03:44 +0200

    Add option for extension of rendered html file

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ module.exports = function(options) {
     // Remember that contents is ALWAYS a buffer
     if (file.isNull()) return cb(null, file); // pass along
     if (file.isStream()) return cb(new Error("gulp-haml: Streaming not supported"));
+    
+    var opts = defaults({
+      extension: '.html'
+    });
 
     var html = haml.render(file.contents.toString("utf8"), options);
-    file.path = gutil.replaceExtension(file.path, '.html');
+    file.path = gutil.replaceExtension(file.path, opts.extension);
     file.contents = new Buffer(html);
 
     cb(null, file);
