2b1e39a82a6ed211e1c4085ed7b16ec3eb06714c
Peter M. Elias <petermelias@gmail.com>
2014-07-24 20:06:33 +0000

    added support for visionmedia HAMLJS compiler and cleaned up tests

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-var map = require('map-stream');
-var rext = require('replace-ext');
-var haml = require('haml');
+(function () {
+  'use strict';
 
-module.exports = function(options) {
-  if(!options) options = {};
-  if(!options.ext) options.ext = '.html';
+  var map = require('map-stream'),
+      rext = require('replace-ext'),
+      util = require('gulp-util'),
+      compilers = {
+        creationix: require('haml'),
+        visionmedia: require('hamljs')
+      };
+      
 
-  // Map each file to this function
-  function hamlStream(file, cb) {
-    // Remember that contents is ALWAYS a buffer
-    if (file.isNull()) return cb(null, file); // pass along
-    if (file.isStream()) return cb(new Error("gulp-haml: Streaming not supported"));
-
-    var html = haml.render(file.contents.toString("utf8"), options);
-    file.path = rext(file.path, options.ext);
-    file.contents = new Buffer(html);
-
-    cb(null, file);
+  function _mergeObjects(o1, o2) {
+    var k, v;
+    for (k in o2) {
+      v = o2[k];
+      o1[k] = v;
+    }
+    return o1;
   }
 
-  // Return a stream
-  return map(hamlStream);
-};
+  module.exports = function(options) {
+    var options = _mergeObjects({
+      ext: '.html',
+      compiler: 'creationix',
+      compilerOpts: {}
+    }, options);
+
+    // Map each file to this function
+    function hamlStream(file, cb) {
+      // Remember that contents is ALWAYS a buffer
+      if (file.isNull()) return cb(null, file); // pass along
+      if (file.isStream()) return cb(new util.PluginError('gulp-haml', 'Streaming not supported'));
+
+      var html = compilers[options.compiler].render(file.contents.toString('utf8'), options.compilerOpts);
+      file.path = rext(file.path, options.ext);
+      file.contents = new Buffer(html);
+
+      cb(null, file);
+    }
+
+    // Return a stream
+    return map(hamlStream);
+  };
+
+})();
+
--- test/main.js
+++ test/main.js
@@ -1,50 +1,58 @@
-var should = require('should');
-var gutil = require('gulp-util');
-var path = require('path');
-var haml = require('../');
-var fs = require('fs');
+(function () {
+  'use strict';
+  var should = require('should'),
+      gutil = require('gulp-util'),
+      path = require('path'),
+      haml = require('../'),
+      fs = require('fs');
 
-require('mocha');
+  require('mocha');
 
-describe('gulp haml', function(){
-  it('should render haml .haml to HTML .html', function(done){
-    var hamlStream = haml();
+  describe('gulp haml', function () {
 
-    var fakeFile = new gutil.File({
-      base: "test/src",
-      cwd: "test/",
-      path: "test/src/haml.haml",
-      contents: fs.readFileSync('test/src/haml.haml')
+    function _fakeFile (name) {
+      return new gutil.File({
+        base: 'test/src',
+        cwd: 'test/',
+        path: 'test/src/haml.haml',
+        contents: fs.readFileSync('test/src/' + name + '.haml')
+      });
+    }
+
+    function _checkFile (name, ext, done) {
+      return function (file) {
+        should.exist(file);
+        should.exist(file.path);
+        should.exist(file.contents);
+        String(path.extname(file.path)).should.equal('.' + ext);
+        String(file.contents).should.equal(fs.readFileSync('test/expected/' + name + '.html', 'utf8'));
+        done();
+      };
+    }
+
+    it('should render haml .haml to HTML .html', function (done){
+      var hamlStream = haml()
+
+      hamlStream.once('data', _checkFile('creationix', 'html', done));
+      hamlStream.write(_fakeFile('creationix'));
     });
 
-    hamlStream.once('data', function(newFile){
-      should.exist(newFile);
-      should.exist(newFile.path);
-      should.exist(newFile.contents);
-      String(newFile.contents).should.equal(fs.readFileSync('test/expected/haml.html', 'utf8'));
-      done();
+    it('should change the extension to .php if defined by opts.ext', function (done) {
+      var hamlStream = haml({
+            ext: '.php'
+          });
+
+      hamlStream.once('data', _checkFile('creationix', 'php', done));
+      hamlStream.write(_fakeFile('creationix'));
     });
-    hamlStream.write(fakeFile);
+
+    it('should switch compilers if specified in options', function (done) {
+      var hamlStream = haml({
+            compiler: 'visionmedia'
+          });
+
+      hamlStream.once('data', _checkFile('visionmedia', 'html', done));
+      hamlStream.write(_fakeFile('visionmedia'));
+    });
   });
-
-  it('should change the extension to .php if defined by opts.ext', function(done){
-    var hamlStream = haml({ext: '.php'});
-
-    var fakeFile = new gutil.File({
-      base: "test/src",
-      cwd: "test/",
-      path: "test/src/haml.haml",
-      contents: fs.readFileSync('test/src/haml.haml')
-    });
-
-    hamlStream.once('data', function(newFile){
-      should.exist(newFile);
-      should.exist(newFile.path);
-      String(path.extname(newFile.path)).should.equal('.php');
-      should.exist(newFile.contents);
-      String(newFile.contents).should.equal(fs.readFileSync('test/expected/haml.html', 'utf8'));
-      done();
-    });
-    hamlStream.write(fakeFile);
-  });
-});
+})();
